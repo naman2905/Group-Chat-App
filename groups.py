3c8490f2b050e8a42819c8475f245a5229d6acce
naman2905 <108929506+naman2905@users.noreply.github.com>
2022-07-09 15:03:13 +0530

    Update groups.py

--- groups.py
+++ groups.py
@@ -27,31 +27,31 @@ async def create_grp(request:Request):
             print(group_check)
             if group_check:
                 response = {
-                "msg":"Group with this name already exists",
+                "message":"Group with this name already exists",
                 "status":True
             }
             else:
                 mydb.groups.insert({"group_name":group_name , "members":members})
                 response = {
-                        "msg":"Group created successfully.",
+                        "message":"Group created successfully.",
                         "status":True
                     }
             
         if not members or not group_name:
             response = {
-                "msg":"Please input members/grp name",
+                "message":"Please input members/grp name",
                 "status":True
             }
             return response
         if not isinstance(members , list) or not isinstance(group_name , str):
             response = {
-                "msg":"Please provide members/group_name in proper format",
+                "message":"Please provide members/group_name in proper format",
                 "status":True
             }
         return response
     except Exception as e:
         response = {
-            "msg":str(e),
+            "message":str(e),
             "status":False
             }
         return response
@@ -64,23 +64,23 @@ def delete_group(request:Request , group_id:str = None):
             deleted_group = mydb.groups.find_one_and_delete({"_id":ObjectId(group_id)}, {"members":1})
             if deleted_group:
                 response = {
-                    "msg":"Group Deleted Successfully.",
+                    "message":"Group Deleted Successfully.",
                     "status":True
                 }
             else:
                 response = {
-                    "msg":"Group not found.",
+                    "message":"Group not found.",
                     "status":True
                 }
         else:
             response = {
-                "msg":"Please provide group id",
+                "message":"Please provide group id",
                 "status":True
             }
         return response
     except Exception as e:
         response = {
-            "msg":str(e),
+            "message":str(e),
             "status":True
             }
         return response
@@ -95,24 +95,24 @@ async def update_group(request:Request , group_id:str= None):
             updated_details = mydb.groups.find_one_and_update({"_id":ObjectId(group_id)} , {"$set":data})
             if updated_details:
                 response = {
-                    "msg":"Data updated successfully",
+                    "message":"Data updated successfully",
                     "data":data, 
                     "success":True
                 }
             else:
                 response = {
-                    "msg":"Group not found.",
+                    "message":"Group not found.",
                     "status":True
                 }
         else:
             response = {
-            "msg":"Provide Group ID", 
+            "message":"Provide Group ID", 
             "success":True
         }
         return response
     except Exception as e:
         response = {
-            "msg":str(e),
+            "message":str(e),
             "success":False 
         }
         return response
@@ -121,7 +121,7 @@ async def update_group(request:Request , group_id:str= None):
 # Group Members APIs
 
 @router.post('/add-member')
-async def create_group(request:Request):
+async def add_member_in_group(request:Request):
     try:
         data = await request.json()
         members = data.get("members" , "")
@@ -141,35 +141,35 @@ async def create_group(request:Request):
                     existing_members_list.get("members").append(i)
                 mydb.groups.find_one_and_update({"_id":ObjectId(group_id)},{"$set":{"members":final_members_to_be_add}})                
                 response = {
-                        "msg":"Participants added successfully.",
+                        "message":"Participants added successfully.",
                         "status":True
                     }
             else:
                 response = {
-                        "msg":"Group does not exists.",
+                        "message":"Group does not exists.",
                         "status":True
                     }
         if not members or not group_id:
             response = {
-                "msg":"Please input members/grp name",
+                "message":"Please input members/grp name",
                 "status":True
             }
             return response
         if not isinstance(members , list) or not isinstance(group_id , str):
             response = {
-                "msg":"Please provide members/group_name in proper format",
+                "message":"Please provide members/group_name in proper format",
                 "status":True
             }
         return response
     except Exception as e:
         response = {
-            "msg":str(e),
+            "message":str(e),
             "status":False
             }
         return response
 
 @router.delete('/delete-member')
-def delete_user(request:Request , user_id:str= None , group_id:str= None):
+def delete_member_from_group(request:Request , user_id:str= None , group_id:str= None):
     try:
         user_id = request.query_params.get("user_id")
         group_id = request.query_params.get("group_id")
@@ -177,29 +177,29 @@ def delete_user(request:Request , user_id:str= None , group_id:str= None):
             deleted_user = mydb.user.find_one_and_delete({"user_id":ObjectId(user_id) , "group_id":ObjectId(group_id)})
             if deleted_user:
                 response = {
-                    "msg":"Deleted user successfully.",
+                    "message":"Deleted user successfully.",
                     "status":True
                 }
             else:
                 response = {
-                    "msg":"User/Group not found.",
+                    "message":"User/Group not found.",
                     "status":True
                 }
         else:
             response = {
-                "msg":"Please provide user id/group id",
+                "message":"Please provide user id/group id",
                 "status":True
             }
         return response
     except Exception as e:
         response = {
-            "msg":str(e),
+            "message":str(e),
             "status":True, 
             }
         return response
 
 @router.get('/get-all-members')
-def get_user_list(request:Request , group_id:str = None):
+def get_all_members(request:Request , group_id:str = None):
     try:
         group_id = request.query_params.get("group_id")
         data = []
@@ -210,24 +210,24 @@ def get_user_list(request:Request , group_id:str = None):
                 data.append(user_detail)
             if user:
                 response = {
-                    "msg":"Here is the complete members list.",
+                    "message":"Here is the complete members list.",
                     "data":data,
                     "status":True
                 }
             else:
                 response = {
-                    "msg":"No member found.",
+                    "message":"No member found.",
                     "status":True
                 }
         else:
             response = {
-                "msg":"Please provide group id",
+                "message":"Please provide group id",
                 "status":True
             }
         return response
     except Exception as e:
         response = {
-            "msg":str(e),
+            "message":str(e),
             "status":True
             }
         return response
@@ -242,26 +242,26 @@ def search_member(request:Request , mobile:int=None , group_id:str=None):
         member = mydb.groups.find_one({"members":{"$all":[user_id['_id']]} , "_id":ObjectId(group_id)})
         if not mobile or not group_id or not user_id:
             response = {
-                "msg":"provide complete data" , 
+                "message":"provide complete data" , 
                 "status":True
             }
             return response
         if member:
             response = {
-                "msg":"user found" , 
+                "message":"user found" , 
                 "data":user_id ,
                 "status":True
             }
             return response
         else:
             response = {
-            "msg":"user not found" , 
+            "message":"user not found" , 
             "status":True
             }
             return response
     except Exception as e:
         response = {
-                "msg":str(e) , 
+                "message":str(e) , 
                 "status":False
         }
         return response
@@ -270,7 +270,7 @@ def search_member(request:Request , mobile:int=None , group_id:str=None):
 # Group Chat APIs
 
 @router.post('/send-messages')
-async def send_msgs(request:Request):
+async def send_messages(request:Request):
     try:
         data = await request.json()
         message = data.get("msg", "")
@@ -297,35 +297,35 @@ async def send_msgs(request:Request):
                     mydb.messages.insert({"_id":msg['msgs'][0],"message":message , "liked":False , "send_at":datetime.datetime.now()})
                     mydb.chat.find_one_and_update({"group_id":ObjectId(group_id)}, {"$set":{"users":users}} , upsert=True)
                 response = {
-                        "msg":"Message send successfully.",
+                        "message":"Message send successfully.",
                         "status":True
                     }
             else:
                 response = {
-                        "msg":"User does not belong to this grp.",
+                        "message":"User does not belong to this grp.",
                         "status":True
                     }
         if not user_id or not message or not group_id:
             response = {
-                "msg":"Please input msg/user id/group_id",
+                "message":"Please input msg/user id/group_id",
                 "status":True
             }
             return response
         if not isinstance(user_id , str) or not isinstance(message , str) or not isinstance(group_id , str):
             response = {
-                "msg":"Please provide user id/message in proper format",
+                "message":"Please provide user id/message in proper format",
                 "status":True
             }
         return response
     except Exception as e:
         response = {
-            "msg":str(e),
+            "message":str(e),
             "status":False
             }
         return response
 
 @router.post('/like-message')
-async def like_msg(request:Request):
+async def like_message(request:Request):
     try:
         data = await request.json()
         message_id = data.get("msg_id" , "")
@@ -337,34 +337,34 @@ async def like_msg(request:Request):
                 msg= mydb.messages.find_one_and_update({"_id":ObjectId(message_id)},{"$set":{"liked":True}})
                 if msg:
                     response = {
-                        "msg":"Liked message Successfully",
+                        "message":"Liked message Successfully",
                         "status":True
                     }
                 else:
                     response = {
-                        "msg":"Wrong msg id",
+                        "message":"Wrong msg id",
                         "status":True
                     }
             else:
                 response = {
-                    "msg":"User does not belong to this group",
+                    "message":"User does not belong to this group",
                     "status":True
                 }
         if not user_id or not message_id or not group_id:
             response = {
-                "msg":"Please input msg/user id/group_id",
+                "message":"Please input msg/user id/group_id",
                 "status":True
             }
             return response
         if not isinstance(user_id , str) or not isinstance(message_id , str) or not isinstance(group_id , str):
             response = {
-                "msg":"Please provide user id/message/group_id in proper format",
+                "message":"Please provide user id/message/group_id in proper format",
                 "status":True
             }
         return response
     except Exception as e:
         response = {
-            "msg":str(e),
+            "message":str(e),
             "status":False
             }
         return response
