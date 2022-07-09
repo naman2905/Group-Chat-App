9963f0da211376beb8468dd1e45f8b758927c5ea
naman2905 <108929506+naman2905@users.noreply.github.com>
2022-07-09 17:16:39 +0530

    Update user.py

--- user.py
+++ user.py
@@ -17,83 +17,83 @@ mydb=client[config("DB_NAME")]
 
 @router.post('/create')
 async def create_user(request:Request):
-    try:    
-        data = await request.json()
-        if data is None:
-            return {"message":"provide data" , "success":True}
-        data["created_at"]=datetime.datetime.now()
-        data["updated_at"]=datetime.datetime.now()
-        mydb.user.insert(data)
-        response = {
-            "message":"user created successfully",
-            "success":True
+    '''
+    API Endpoint for creating user by admin
+    body = {
+            "name":name,
+            "mobile":mobile,
+            "email":email
         }
-        return response
+    '''   
+    data = await request.json()
+    if data is None:
+        return {"message":"provide data" , "success":True}
+    data["created_at"]=datetime.datetime.now()
+    data["updated_at"]=datetime.datetime.now()
+    mydb.user.insert(data)
+    response = {
+        "message":"user created successfully",
+        "success":True
+    }
+    return response
         
-    except Exception as e:
-        response = {
-            "message":str(e),
-            "success":False 
-        }
-        return response
-
 @router.patch('/update')
 async def update_user(request:Request , user_id:str=None):
-    try:    
-        user_id = request.query_params.get("user_id")
-        data = await request.json()
-        data["updated_at"]=datetime.datetime.now()
-        if user_id:
-            updated_user = mydb.user.find_one_and_update({"_id":ObjectId(user_id)} , {"$set":data})
-            if updated_user:
-                response = {
-                    "message":"Data updated successfully",
-                    "data":data, 
-                    "success":True
-                }
-            else:
-                response = {
-                    "message":"User not found.",
-                    "status":True
-                }
+    '''
+    API Endpoint for updating user by admin
+    body = {
+            "name":name,
+            "email":email
+        }
+    '''
+    user_id = request.query_params.get("user_id")
+    data = await request.json()
+    data["updated_at"]=datetime.datetime.now()
+    if user_id:
+        updated_user = mydb.user.find_one_and_update({"_id":ObjectId(user_id)} , {"$set":data})
+        if updated_user:
+            response = {
+                "message":"Data updated successfully",
+                "data":data, 
+                "success":True
+            }
         else:
             response = {
-            "message":"User does not exists/ Incorrect User ID", 
-            "success":True
-        }
-        return response
-    except Exception as e:
+                "message":"User not found.",
+                "status":True
+            }
+    else:
         response = {
-            "message":str(e),
-            "success":False 
-        }
-        return response
+        "message":"User does not exists/ Incorrect User ID", 
+        "success":True
+    }
+    return response
 
 @router.delete('/delete')
 def delete_user(request:Request , user_id:str=None):
-    try:
-        user_id = request.query_params.get("user_id")
-        if user_id:
-            deleted_user = mydb.user.find_one_and_delete({"_id":ObjectId(user_id)})
-            if deleted_user:
-                response = {
-                    "message":"Deleted user successfully.",
-                    "status":True
-                }
-            else:
-                response = {
-                    "message":"User not found.",
-                    "status":True
-                }
-        else:
+    '''
+    API Endpoint for deleting user by admin
+    query_params = {
+            "user_id":user_id
+        }
+    '''
+    user_id = request.query_params.get("user_id")
+    if user_id:
+        deleted_user = mydb.user.find_one_and_delete({"_id":ObjectId(user_id)})
+        if deleted_user:
             response = {
-                "message":"Please provide user id",
+                "message":"Deleted user successfully.",
                 "status":True
             }
-        return response
-    except Exception as e:
-        response = {
-            "message":str(e),
-            "status":True
+        else:
+            response = {
+                "message":"User not found.",
+                "status":True
             }
-        return response
+    else:
+        response = {
+            "message":"Please provide user id",
+            "status":True
+        }
+    return response
+
